8c368eea68b00b4d397ef9aa98d77995e725a2d4
sujin0529 <201802150@o.cnu.ac.kr>
2020-11-20 10:43:57 +0900

    13424_비밀_모임

--- Java/13424_비밀_모임/13424_Floyd_Warshall.java
+++ Java/13424_비밀_모임/13424_Floyd_Warshall.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Scanner sc = new Scanner(System.in);
 		int testCase = sc.nextInt();
 		for(int index=0; index<testCase; index++) {
